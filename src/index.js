314fc23ced2b1e6dc957afe0b8f165025ce88521
vannizhang <vannizhang@gmail.com>
2018-09-14 16:16:24 -0700

    get active storm names

--- src/index.js
+++ src/index.js
@@ -6,23 +6,34 @@ import * as calcite from 'calcite-web';
 
 // import style files
 import "./style/index.scss";
+import { rejects } from 'assert';
 
 // import other files
 
 // app configs
 const ITEM_ID_HURRICANE_WEB_MAP = 'c500379cb42d4ba28407502563505e3e'; 
+const LAYER_ID_FORECAST_POSITION = '0';
+const LAYER_ID_OBSERVED_POSITION = '1';
+
+const URL_HURRICANE_LAYER = 'https://utility.arcgis.com/usrsvcs/servers/4693f2a1d2e348c193ce5ec4d1d887a5/rest/services/LiveFeeds/Hurricane_Active/MapServer';
+
+const FIELD_NAME_STORM_NAME = 'STORMNAME';
+const FIELD_NAME_STORM_ID = 'STORMID';
 
 const DOM_ID_MAP_VIEW_CONTAINER = 'viewDiv';
 
 
 esriLoader.loadModules([
     'esri/views/MapView', 
-    'esri/WebMap'
+    'esri/WebMap',
+    "esri/request"
 ]).then(([
     MapView, 
-    WebMap
+    WebMap,
+    esriRequest
 ])=>{
 
+    // map view and controls
     const HurricaneMap = function(){
 
         // private variables
@@ -58,17 +69,101 @@ esriLoader.loadModules([
 
     const AppDataModel = function(){
 
-        let stormName = '';
+        let visibleStormName = '';
+        let activeStormNames = null; // lookup table for all active storm names
         let observedPosition = null;
         let forecastPosition = null;
 
-        const setStormName = (name='')=>{
-            stormName = name;
+        const init = ()=>{
+            queryActiveStoreNames();
         };
 
-        const getStormname = ()=>{
-            return stormName
+        const setVisibleStormName = (name='')=>{
+            visibleStormName = name;
         };
+
+        const setActiveStormNames = (features=[])=>{
+
+            activeStormNames = features.map(d=>{
+                const stormName = d.attributes[FIELD_NAME_STORM_NAME];
+                return {
+                    key: stormName.toLowerCase(),
+                    value: stormName,
+                };
+            });
+
+            console.log(activeStormNames);
+        };
+
+        const setObservedPosition = ()=>{
+
+        };
+
+        const setForecastPosition = ()=>{
+
+        };
+
+        const getVisibleStormName = ()=>{
+            return visibleStormName;
+        };
+
+        const queryActiveStoreNames = ()=>{
+            const LayerURL = URL_HURRICANE_LAYER + '/' + LAYER_ID_FORECAST_POSITION;
+
+            const queryParam = {
+                where: '1=1',
+                outFields: FIELD_NAME_STORM_NAME,
+                returnDistinctValues: true,
+                returnGeometry: false,
+                f: 'json',
+            };
+
+            queryFeatures(LayerURL, queryParam).then(res=>{
+                console.log('distinct storm names', res);
+                setActiveStormNames(res.features);
+            }).catch(err=>{
+                console.error("cannot get distinct storm names", err);
+            });
+        };
+
+        const queryObservedPosition = ()=>{
+
+        };
+
+        const queryForecastPosition = ()=>{
+
+        };
+
+        const queryFeatures = (layerURL, queryParams)=>{
+            const requestUrl = layerURL +  '/query';
+
+            const options = { 
+                responseType: "json" 
+            };
+
+            if(queryParams){
+                options.query = queryParams;
+            }
+
+            return new Promise((resolve, reject)=>{
+                
+                esriRequest(requestUrl, options).then(function(response){
+                    // The requested data
+                    // console.log(response);
+                    if(response.data){
+                        resolve(response.data);
+                    } 
+                }).catch(error=>{
+                    // console.error("query operation is failed", error);
+                    reject(error);
+                })
+            });
+        };
+
+        return {
+            init: init
+        }
+
     };
 
     const AppView = function(){
@@ -79,10 +174,19 @@ esriLoader.loadModules([
 
     };
 
+    const Helper = function(){
+        this.capitalizeFirstLetter= (string)=>{
+            string = string.toLowerCase();
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        };
+    };
+
     // initiate core modules
     const dataModel = new AppDataModel();
     const hurricaneMap = new HurricaneMap();
+    const helper = new Helper();
 
+    dataModel.init();
 
 }).catch(err => {
     // handle any errors
