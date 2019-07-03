2d9823736a9235692ccefca25195b4a19ef2ab6c
Jinnan Zhang <vannizhang@gmail.com>
2019-07-02 17:01:28 -0700

    update wind gust config

--- src/data/WindGustLayerConfig.js
+++ src/data/WindGustLayerConfig.js
@@ -8,77 +8,77 @@ export default {
     {
       "value": "1",
       "label": "Light Air",
-      "description": ">=1 <=3 mph"
+      "description": "<=3 mph"
     },
     {
       "value": "2",
       "label": "Light Breeze",
-      "description": ">3 <=7 mph"
+      "description": "<=7 mph"
     },
     {
       "value": "3",
       "label": "Gentle Breeze",
-      "description": ">7 <=12 mph"
+      "description": "<=12 mph"
     },
     {
       "value": "4",
       "label": "Moderate Breeze",
-      "description": ">12 <=17 mph"
+      "description": "<=17 mph"
     },
     {
       "value": "5",
       "label": "Fresh Breeze",
-      "description": ">17 <=24 mph"
+      "description": "<=24 mph"
     },
     {
       "value": "6",
       "label": "Strong Breeze",
-      "description": ">24 <=30 mph"
+      "description": "<=30 mph"
     },
     {
       "value": "7",
       "label": "Moderate Gale",
-      "description": ">30 <=38 mph"
+      "description": "<=38 mph"
     },
     {
       "value": "8",
       "label": "Fresh Gale",
-      "description": ">38 <=46 mph"
+      "description": "<=46 mph"
     },
     {
       "value": "9",
       "label": "Strong Gale",
-      "description": ">46 <=54 mph"
+      "description": "<=54 mph"
     },
     {
       "value": "10",
       "label": "Storm",
-      "description": ">54 <=63 mph"
+      "description": "<=63 mph"
     },
     {
       "value": "11",
       "label": "Violent Storm",
-      "description": ">63 <=73 mph"
+      "description": "<=73 mph"
     },
     {
       "value": "12",
       "label": "Cat-1 Hurricane",
-      "description": ">73 <=95 mph"
+      "description": "<=95 mph"
     },
     {
       "value": "13",
       "label": "Cat-2 Hurricane",
-      "description": ">95 <=110 mph"
+      "description": "<=110 mph"
     },
     {
       "value": "14",
       "label": "Cat-3 Hurricane",
-      "description": ">110 <=130 mph"
+      "description": "<=130 mph"
     },
     {
       "value": "15",
       "label": "Cat-4 Hurricane",
-      "description": ">130 <=155 mph"
+      "description": "<=155 mph"
     },
     {
       "value": "16",
