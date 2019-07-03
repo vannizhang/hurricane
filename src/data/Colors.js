7bf2f34a598b4b89619aac160c950640404bc471
Jinnan Zhang <vannizhang@gmail.com>
2019-07-03 15:22:26 -0700

    update info chart colors

--- src/data/Colors.js
+++ src/data/Colors.js
@@ -1,9 +1,9 @@
 export default {
-    'Speak Only English': '#015175',
-    'Other Languages': '#07A6A4',
-    '< 18 yrs': '#59CCD8',
-    '18-64 yrs': '#166B8C',
-    '+ 65 yrs': '#061E33',
+    'Speak Only English': '#3DE0F2',
+    'Other Languages': '#124566',
+    '< 18 yrs': '#30CFF2',
+    '18-64 yrs': '#2097DB',
+    '+ 65 yrs': '#18608C',
     'pinDrop': '#FFFFFF',
     'sidebar': 'rgba(0,48,77,.75)',
     'chartLabelText': 'rgba(255,255,255,.8)'
